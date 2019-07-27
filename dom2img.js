31e4724fff1d757abcc82709fed137289366ea21
trebleC <kiffong181@gmail.com>
2019-07-27 18:43:17 +0800

    fix style

---
+++ dom2img.js
@@ -0,0 +1,15 @@
+
+
+
+
+var node = document.getElementById('container');
+
+domtoimage.toPng(node)
+    .then(function (dataUrl) {
+        var img = new Image();
+        img.src = dataUrl;
+        document.body.appendChild(img);
+    })
+    .catch(function (error) {
+        console.error('oops, something went wrong!', error);
+    });
